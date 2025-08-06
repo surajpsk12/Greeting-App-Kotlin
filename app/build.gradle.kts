eb35ab87b06d71d82f67c0793c7e4a7e404accd3
Suraj Kumar <sk658139@gmail.com>
2025-08-06 14:51:28 +0530

    added icon of app and some ui changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
